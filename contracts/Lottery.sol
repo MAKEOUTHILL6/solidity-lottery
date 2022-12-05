f4929f18a53f1b0bd6e3c8cfdf567b06d6be220d
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-12-05 23:29:53 +0200

    VRFConsumerBaseV2 inherited and constructor implemented

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,18 +2,20 @@
 
 pragma solidity 0.8.17;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughETHEntered();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     // State Variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_participants;
 
-    // Events 
+    // Events
 
     event RaffleEnter(address indexed participant);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinator, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
     }
 
@@ -22,20 +24,24 @@ contract Lottery {
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughETHEntered();
         }
-        
+
         s_participants.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function pickRandomWinner() external {}
 
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 
     // View / Pure Functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getParticipantByIndex(uint256 index) public view returns(address){
+    function getParticipantByIndex(
+        uint256 index
+    ) public view returns (address) {
         return s_participants[index];
     }
 }
